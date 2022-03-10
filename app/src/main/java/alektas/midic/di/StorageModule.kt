270be36295e0f067f1d98081c021f9569b171c85
Alexander Totilas <totilas.aleks@gmail.com>
2022-03-10 09:29:46 +0300

    [33] fix: bookmark DAO missed in DI storage module;

--- app/src/main/java/alektas/midic/di/StorageModule.kt
+++ app/src/main/java/alektas/midic/di/StorageModule.kt
@@ -1,6 +1,8 @@
 package alektas.midic.di
 
 import alektas.common.data.local.db.AppDatabase
+import alektas.common.data.local.db.dao.BookmarksDao
+import alektas.core.di.qualifiers.ApplicationContext
 import android.content.Context
 import androidx.room.Room
 import dagger.Module
@@ -17,9 +19,14 @@ interface StorageModule {
         @Provides
         @Singleton
         fun provideAppDatabase(
+            @ApplicationContext
             context: Context
         ): AppDatabase = Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME)
             .build()
+
+        @Provides
+        fun provideBookmarkDao(database: AppDatabase): BookmarksDao = database.bookmarksDao()
+
     }
 
 }
